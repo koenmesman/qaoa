3d08dae42fd8f32437bf34c6665a21bbfdb8ecaf
koenmesman <koenmesman@gmail.com>
2021-03-24 10:29:27 +0100

    stylistic update

--- python/Problem_set.py
+++ python/Problem_set.py
@@ -90,24 +90,27 @@ def create_dir(new_dir):
     try:
         if not os.path.exists(path):
             os.makedirs(path, 0o700)
+            print("Successfully created the directory %s " % path)
     except OSError:
         print("Creation of the directory %s failed" % path)
-    else:
-        print("Successfully created the directory %s " % path)
+
 
     path = "problem_sets" + str(new_dir)
     try:
         if not os.path.exists(path):
             os.makedirs(path, 0o700)
+            print("Successfully created the directory %s " % path)
     except OSError:
         print("Creation of the directory %s failed" % path)
-    else:
-        print("Successfully created the directory %s " % path)
 
 
 # transforms the graph to a weighted graph with edge value inf for disconnected vertices
-def to_tsp(edge_list):
-    n = max(edge_list, key=lambda x: x[1])[1]+1
+def tsp_problem_set(n, method, *arg):
+    if len(arg) == 1:
+        p = arg[0]
+        edge_list = method(n, p)
+    else:
+        edge_list = method(n)
     full_list = fully_connected(n)
     for i in range(len(full_list)):
         if full_list[i] not in edge_list:
@@ -118,28 +121,40 @@ def to_tsp(edge_list):
 
 
 # generate a file with size 5 to n vertices with selected method
-def generate_problem(method, n, *arg):
+def generate_problem_set(method, n, *arg):
+    try:
+        create_dir("/"+method.__name__)
+        edges = []
 
-    create_dir("/"+method.__name__)
-    edges = []
+        if len(arg) == 1:
+            for i in range(5, n + 1):
+                p = arg[0]
+                edges = method(n, p)
+                path = "problem_sets/"+method.__name__+"/test_" + str(i) + ".dat"
+                np.savetxt(path, edges)
 
-    if len(arg) == 1:
-        for i in range(5, n + 1):
-            p = arg[0]
-            edges = method(n, p)
-            path = "problem_sets/"+method.__name__+"/test_" + str(i) + ".dat"
-            np.savetxt(path, edges)
+        else:
+            for i in range(5, n+1):
 
-    else:
-        for i in range(5, n+1):
-
-            edges = method(n)
-            path = "problem_sets/"+method.__name__+"/test_" + str(i) + ".dat"
-            np.savetxt(path, edges)
-
+                edges = method(n)
+                path = "problem_sets/"+method.__name__+"/test_" + str(i) + ".dat"
+                np.savetxt(path, edges)
+        print("Problem set created with method", method.__name__, "for sizes 5 to", n, ".")
+    except:
+        print("Something went wrong. Use this:\nUnweighted graphs: generate_problem_set(method, (nr. of max vertices),"
+              "(optionalargument e.g. probability of ratio)) \n"
+              "Methods:\n"
+              "set_density(n, r)\n"
+              "include_all(n, r)\n"
+              "set_probability(n, p)\n"
+              "regular_graph(n)\n"
+              "fully_connected(n)\n"
+              "Tsp weighted graph: generate_problem_set(method, (nr. of max vertices), method,"
+              "(optionalargument e.g. probability of ratio))")
     return edges
 
 
 # examples:
-graph = generate_problem(regular_graph, 6)
-tsp_graph = to_tsp(graph)
+generate_problem_set(tsp_problem_set, 10, regular_graph)
+generate_problem_set(include_all, 10, 2)
+
